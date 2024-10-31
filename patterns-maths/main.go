83bbb56d892a98ce66816adc14d0ef05ff2c679e
vamsikrishnasiddu <53222840+vamsikrishnasiddu@users.noreply.github.com>
2024-10-31 06:55:00 +0000

    added pattern6

--- patterns-maths/main.go
+++ patterns-maths/main.go
@@ -9,7 +9,8 @@ func main() {
 	//pattern2()
 	//pattern3()
 	//pattern4()
-	pattern5()
+	//pattern5()
+	pattern6()
 }
 
 /*
@@ -39,17 +40,17 @@ func pattern1() {
 ***
 ****
 *****
-*/
+ */
 
-func pattern2(){
+func pattern2() {
 
 	// outer to loop to repeat the inner loop 5 times.
 
-	for i:=0;i<5;i++{
-		// inner loop will iterate upto i 
+	for i := 0; i < 5; i++ {
+		// inner loop will iterate upto i
 		// it will print the '*' in each iteration.
 
-		for j:=0;j<=i;j++{
+		for j := 0; j <= i; j++ {
 			fmt.Print("*")
 		}
 		fmt.Println()
@@ -64,14 +65,14 @@ func pattern2(){
 12345
 */
 
-func pattern3(){
+func pattern3() {
 	// outer loop to iterate  5 times.
 
-	for i:=0;i<5;i++{
+	for i := 0; i < 5; i++ {
 		// inner loop to iterate upto i.
 		// it will print the numbers using j+1
-		for j:=0;j<=i;j++{
-			fmt.Print(j+1)
+		for j := 0; j <= i; j++ {
+			fmt.Print(j + 1)
 		}
 		fmt.Println()
 	}
@@ -85,15 +86,15 @@ func pattern3(){
 55555
 */
 
-func pattern4(){
+func pattern4() {
 	// outer loop iterates 5 times.
-	 for i:=0;i<5;i++{
-		 // inner loop will iterate upto i
-		 for j:=0;j<=i;j++{
-			 fmt.Print(i+1)
-		 }
-		 fmt.Println()
-	 }
+	for i := 0; i < 5; i++ {
+		// inner loop will iterate upto i
+		for j := 0; j <= i; j++ {
+			fmt.Print(i + 1)
+		}
+		fmt.Println()
+	}
 }
 
 /*
@@ -103,22 +104,40 @@ func pattern4(){
 ***
 **
 *
-*/
+ */
 
-func pattern5(){
-	n:=5
+func pattern5() {
+	n := 5
 	// outer loop iterates 5 times.
-	for i:=0;i<n;i++{
+	for i := 0; i < n; i++ {
 		// inner loop goes upto n-i
-		for j:=0;j<n-i;j++{
-			 fmt.Print("*")
-			
+		for j := 0; j < n-i; j++ {
+			fmt.Print("*")
+
 		}
 		fmt.Println()
 	}
 }
 
+/*
+12345
+1234
+123
+12
+1
+*/
 
+func pattern6() {
+	n := 5
+	// outer loop iterates 5 times.
+	for i := 0; i < n; i++ {
+		// inner loop goes upto n-i
+		// it will print the numbers using j+1
+		for j := 0; j < n-i; j++ {
+			fmt.Print(j + 1)
+		}
 
+		fmt.Println()
 
-
+	}
+}
